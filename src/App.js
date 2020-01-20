9b98943b374ed4384540a438a6bd3045ca778707
Nikolas Santis <nikosantis@gmail.com>
2020-01-19 23:01:30 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Link } from "react-router-dom"
+import Home from './pages/Home'
+import Recipe from './pages/Recipe'
+import Timer from './pages/Timer'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <header>
+            <Link to="/">Recetas</Link>
+          </header>
+
+          <main>
+            <Route exact path="/" component={Home} />
+            <Route path="/recipe/:recipeId" component={Recipe} />
+            <Route path="/timer" component={Timer} />
+          </main>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
